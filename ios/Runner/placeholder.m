b0968d23bb4e18e4f4f3143442e81b67cffe9d42
ghifari <ghifariakbar2018@gmail.com>
2023-09-26 15:30:12 +0700

    ios files

---
+++ ios/Runner/placeholder.m
@@ -0,0 +1,8 @@
+//
+//  placeholder.m
+//  Runner
+//
+//  Created by Ghifari on 26/09/23.
+//
+
+#import <Foundation/Foundation.h>
---
+++ ios/placeholder.m
@@ -0,0 +1,8 @@
+//
+//  placeholder.m
+//  
+//
+//  Created by Ghifari on 26/09/23.
+//
+
+#import <Foundation/Foundation.h>
